23233825887bba4d60db43bf6af633608db2bf26
arlen <arlen.stwar.electrico@gmail.com>
2018-06-11 13:49:03 -0500

    borrar stwar js

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
-import {
-  AppRegistry,
-} from 'react-native';
-import App from "./stwar";
-AppRegistry.registerComponent('todo', () => App);
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Header from "./header";
+import Footer from "./footer";
+export default class App extends React.Component {
+  render() {
+    return (
+    <View style={styles.container}>
+        <Header />
+        <View style={styles.content } >
+            
+        </View>
+        <Footer />
+    </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#F5F5F5"
+    },
+    content: {
+      flex: 1
+    }
+})
